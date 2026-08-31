f7980eb86e2117b94be3da697a6ef4cf36dda683
Dafneé Moreno <dafneemoreno.16@gmail.com>
2026-08-31 16:33:44 -0600

    feat(parcial01) - add db initializer

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -16,6 +16,22 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) { }
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>(); // Para ejecutar la migración de la BD
+        await context.Database.MigrateAsync(); // Este método es onligatorio, aplica cualquier migración pendiente para el contexto de la BD
+        await DbInitializer.SeedDataAsync(context); 
+    }
+
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error ocurred during migration/seeding");
+}
+
+
 app.MapControllers();
 
 app.Run();
