244b1d6b353ba28403daa081551e92aee73aeb0e
dfnmor <dafneemoreno.16@gmail.com>
2026-09-09 07:13:13 -0600

    feat(parcial02) - add CORS

--- web/EventsHub/src/App.tsx
+++ web/EventsHub/src/App.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/v1/events')
+      .then(response => response.json())
+      .then(data => setActivities(data));
+
+    return () => {};
+  }, []);
 
   return (
-   <h3>Events Hub</h3>
+    <div>
+      <h3 style={{ color: 'red' }}>Events Hub</h3>
+      <ul>
+        {activities.map((activity: Activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
 export default App
+ 
