153c9e1d5f0418e61c4c9abb29db36e103e43cf8
dfnmor <dafneemoreno.16@gmail.com>
2026-09-09 07:13:13 -0600

    feat(parcial01) - add corse

--- web/EventsHub/src/App.tsx
+++ web/EventsHub/src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() =>{
+    fetch('https://localhost:5001/api/v1/events')
+      .then(response => response.json())
+      .then(data => setActivities(data));
+
+    return() => {};
+  }, []);
 
   return (
-   <h3>Events Hub</h3>
+    <div>
+      <h3 style ={{ color: 'red' }}>Events Hub</h3>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
